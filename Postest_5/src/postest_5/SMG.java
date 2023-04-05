87e32715016a416397f9978ed1b72d33e8cd790e
Pramox71 <93047322+Pramox71@users.noreply.github.com>
2023-04-05 23:53:06 +0800

    Add files via upload

---
+++ Postest_5/src/postest_5/SMG.java
@@ -0,0 +1,48 @@
+package postest_5;
+
+public class SMG extends Senjata{
+    private final String Jenis = "SMG";
+    private String kode;
+    private int pelor;
+
+    public SMG(String Nama, int stok, String kode, int pelor) {
+        super(Nama, stok);
+        this.kode = kode;
+        this.pelor = pelor;
+    }
+    
+    public String getJenis() {
+        return Jenis;
+    }
+
+    public int getPelor() {
+        return pelor;
+    }
+
+    public void setPelor(int pelor) {
+        this.pelor = pelor;
+    }
+
+    public String getKode() {
+        return kode;
+    }
+
+    public void setKode(String kode) {
+        this.kode = kode;
+    }
+    
+     @Override
+    public final void display(){
+        System.out.printf("|%-21s|%-23s|%-10s|%-8s|", Nama_Senjata, Jenis, kode, Stok);
+        System.out.printf("\n");
+    }
+    
+    public void display(boolean Detail){
+        if (Detail)display();
+        else{
+            System.out.printf("|%-21s|%-8s|", Nama_Senjata, Stok);
+            System.out.printf("\n");
+        }
+    }
+    
+}
---
+++ Postest_5/src/postest_5/Senjata.java
@@ -0,0 +1,29 @@
+package postest_5;
+
+public abstract class Senjata {
+    protected String Nama_Senjata;
+    protected int Stok;
+    
+    public Senjata(String Nama, int stok){
+        this.Nama_Senjata =  Nama;
+        this.Stok = stok;
+    }
+
+    public void setNama_Senjata(String Nama_baru){
+        this.Nama_Senjata = Nama_baru;
+    }
+    public void setStok(int stok_baru){
+        this.Stok =  stok_baru;
+    }
+    
+    public String getNama_Senjata(){
+        return Nama_Senjata;
+    }
+    public int getStok() {
+        return Stok;
+    }
+    
+    
+    public abstract void display();
+    
+}
